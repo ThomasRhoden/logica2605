ba2a616460afa9bf8c5b6a9d236982b4900a9d98
ThomasRhoden <rhodenthomas@gmail.com>
2026-05-26 19:11:03 -0300

    Commit inicial

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    int soma = 0;
+
+    for(int i = 1; i <= 20; i++){
+        if(i % 2 == 0){
+            soma += i;
+        }
+    }
+
+    std::cout<<"Soma dos pares de 1 a 20: "<<soma<<std::endl;
+    
+    return 0;
+}
