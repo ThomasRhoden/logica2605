2e6d89420c2d5145feaeb58f6df7595e70b22af2
ThomasRhoden <rhodenthomas@gmail.com>
2026-05-26 19:59:30 -0300

    Commit quarto

---
+++ main4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int quadrado(int n)
+{
+    return n * n;
+}
+int main()
+{
+    std::cout << "Quadrado de 5: " << quadrado(5) << std::endl;
+    std::cout << "Quadrado de 8: " << quadrado(8) << std::endl;
+    std::cout << "Quadrado de 10: " << quadrado(10) << std::endl;
+
+    return 0;
+}
