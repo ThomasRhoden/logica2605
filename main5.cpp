540c380bd2b8cfb32c7ae4a7033b9248cf21e11d
ThomasRhoden <rhodenthomas@gmail.com>
2026-05-26 20:05:13 -0300

    Cubo atualizado

--- main5.cpp
+++ main5.cpp
@@ -6,9 +6,9 @@ int AoCubo(int n)
 }
 int main()
 {
-    std::cout << "Quadrado de 5: " << AoCubo(5) << std::endl;
-    std::cout << "Quadrado de 8: " << AoCubo(8) << std::endl;
-    std::cout << "Quadrado de 10: " << AoCubo(10) << std::endl;
+    std::cout << "Cubo de 5: " << AoCubo(5) << std::endl;
+    std::cout << "Cubo de 8: " << AoCubo(8) << std::endl;
+    std::cout << "Cubo de 10: " << AoCubo(10) << std::endl;
 
     return 0;
 }
