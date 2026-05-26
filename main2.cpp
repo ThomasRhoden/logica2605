c9b411bb335021bd71f2c5a1207648669aba5087
ThomasRhoden <rhodenthomas@gmail.com>
2026-05-26 19:19:24 -0300

    Commit segundo

---
+++ main2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    int numero;
+    int contador = 0;
+
+    while (true)
+    {
+        std::cout << "Digite um numero (0 para parar): ";
+        std::cin >> numero;
+
+        if (numero == 0)
+        {
+            break;
+        }
+
+        contador++;
+    }
+
+    std::cout << "Voce digitou " << contador << " numeros!" << std::endl;
+
+    return 0;
+}
