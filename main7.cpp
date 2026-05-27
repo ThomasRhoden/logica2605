384444269fc4ced96842591a6d71f1f3cb59362f
ThomasRhoden <rhodenthomas@gmail.com>
2026-05-26 21:09:22 -0300

    Commit setimo

---
+++ main7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int numeros[5];
+    int soma = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << "Digite o numero: " << (i + 1) << ": " <<std::endl;
+        std::cin >> numeros[i];
+        soma += numeros[i];
+    }
+
+    std::cout << "Soma: " << soma << std::endl;
+}
